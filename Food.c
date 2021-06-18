474d511e541f9d0895fef3e99bf635d8672f7480
5hezan <75679579+5hezan@users.noreply.github.com>
2021-06-19 01:00:30 +0530

    Add files via upload

---
+++ Food.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int a;
+    printf("\nEnter a number for food item   :");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+        printf("Pizza\nRs 239");
+        break;
+        case 2:
+        printf("burger\nRs 129");
+        break;
+        case 3:
+        printf("Pasta\nRs 179");
+        break;
+        case 4:
+        printf("French fries\nRs 99");
+        break;
+        case 5:
+        printf("Sandwich\nRs 149");
+        break;
+        }
+        }
