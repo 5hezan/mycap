e0f37c6ec87d2f4bd5f963212f43dc7dcff041e3
5hezan <75679579+5hezan@users.noreply.github.com>
2021-07-07 17:10:13 +0530

    Add files via upload

---
+++ shez2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+        float a,b,c;
+        std::cout<<"\nEnter the first number    :";
+        std::cin>>a;
+        std::cout<<"\nEnter the second number      :";
+        std::cin>>b;
+        c=a*b;
+        std::cout<<"\nThe product of "<<a<<" and "<<b<<" is "<<c;
+        return 0;
+}
