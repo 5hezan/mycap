1b197a3cd8ab17539eaa114972e74b25da754238
5hezan <75679579+5hezan@users.noreply.github.com>
2021-07-07 17:01:58 +0530

    Add files via upload

---
+++ shez1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+    std::cout<<"\nThe size of int is   :"<<sizeof(a);
+    std::cout<<"\nThe size of float is   :"<<sizeof(b);
+    std::cout<<"\nThe size of double is   :"<<sizeof(c);
+    std::cout<<"\nThe size of char is   :"<<sizeof(d);
+    return 0;
+}
